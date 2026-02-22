98c0d49ff15d1a8db20b335f978a21f361e3e802
abubakar308 <mdabubakarsiddique789@gmail.com>
2026-02-22 23:15:39 +0600

    seedAdmin created

---
+++ src/seed/seedAdmin.ts
@@ -0,0 +1,37 @@
+import bcrypt from "bcrypt"
+import { Role } from "../../generated/prisma/enums";
+import { prisma } from "../lib/prisma";
+
+const seedAdmin = async () => {
+  const hashedPassword = await bcrypt.hash("admin12", 8);
+
+  const adminData = {
+    name: "Admin",
+    email: "admin@gmail.com",
+    role: Role.ADMIN,
+    password: hashedPassword,
+  };
+
+
+  try {
+    const isExists = await prisma.user.findUnique({
+      where: {
+        email: adminData.email,
+      },
+    });
+
+    if (isExists) {
+      console.log("Admin already exists!!");
+      return;
+    }
+    const admin = await prisma.user.create({
+      data: adminData,
+    });
+    console.log("Admin created successfully!!");
+  } catch (error) {
+    console.log(error);
+  } finally{
+    await prisma.$disconnect()
+  }
+};
+seedAdmin();
